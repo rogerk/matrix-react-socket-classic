2c3fb427f1d05846ab5407d6440bc489ffce3dcf
rogerk <rkitain@yahoo.com>
2019-06-21 13:18:37 -0400

    Constants used for react redux actions.

---
+++ matrix/socket-io-client/src/js/constants/action-types.js
@@ -0,0 +1,5 @@
+export const ALL_MATRIX = "ALL_MATRIX";
+export const ALL_MATRIX_FAILURE = "ALL_MATRIX_FAILURE";
+export const INITIAL_MATRIX = "INITIAL_MATRIX";
+export const SET_COLOR = "SET_COLOR";
+export const PIXEL_COLOR_UPDATE = "PIXEL_COLOR_UPDATE";
