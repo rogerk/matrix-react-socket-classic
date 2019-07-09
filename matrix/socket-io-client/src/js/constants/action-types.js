1f1f8bc308c0d7514982d628ca20045025460ea0
rogerk <rkitain@yahoo.com>
2019-07-09 16:46:45 -0400

    - added RESET action type.

--- matrix/socket-io-client/src/js/constants/action-types.js
+++ matrix/socket-io-client/src/js/constants/action-types.js
@@ -1,5 +1,6 @@
 export const ALL_MATRIX = "ALL_MATRIX";
 export const ALL_MATRIX_FAILURE = "ALL_MATRIX_FAILURE";
 export const INITIAL_MATRIX = "INITIAL_MATRIX";
+export const RESET = "RESET";
 export const SET_COLOR = "SET_COLOR";
 export const PIXEL_COLOR_UPDATE = "PIXEL_COLOR_UPDATE";
