63787ca22595b5ca363bd156f0e9ef0bc2e79ab4
rogerk <rkitain@yahoo.com>
2019-07-19 08:14:56 -0400

    - initial socket middleware

--- matrix/socket-io-client/src/js/middleware/socketSendReceive.js
+++ matrix/socket-io-client/src/js/middleware/socketSendReceive.js
@@ -2,6 +2,7 @@ import io from "socket.io-client";
 
 const socketSendReceive = store => next => action => {
     const socket = io();
+    next(action);
 };
 
 export default socketSendReceive;
--- matrix/socket-io-client/src/js/store/index.js
+++ matrix/socket-io-client/src/js/store/index.js
@@ -1,7 +1,8 @@
 import socketSendReceive from "../middleware/socketSendReceive";
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 import Reducers from "../reducers";
 
-const store = createStore(Reducers, applyMiddleware(socketSendReceive));
+const store = createStore(Reducers, applyMiddleware(socketSendReceive, thunk));
 
 export default store;
