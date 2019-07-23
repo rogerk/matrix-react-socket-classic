449f580f9aa5653307a64d05ad4028b78d5ae1fe
rogerk <rkitain@yahoo.com>
2019-07-23 17:18:42 -0400

    - added UPDATE_PIXEL_COLOR type

--- matrix/socket-io-client/src/js/constants/action-types.js
+++ matrix/socket-io-client/src/js/constants/action-types.js
@@ -4,3 +4,4 @@ export const INITIAL_MATRIX = "INITIAL_MATRIX";
 export const RESET = "RESET";
 export const SET_COLOR = "SET_COLOR";
 export const PIXEL_COLOR_UPDATE = "PIXEL_COLOR_UPDATE";
+export const UPDATE_PIXEL_COLOR = "UPDATE_PIXEL_COLOR";
