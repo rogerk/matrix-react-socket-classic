1417538b86f104816be2029c94143fb73cc2579e
rogerk <rkitain@yahoo.com>
2019-07-23 17:19:54 -0400

    - added event handler/listening

--- matrix/socket-io-client/src/js/middleware/socketSendReceive.js
+++ matrix/socket-io-client/src/js/middleware/socketSendReceive.js
@@ -1,8 +1,26 @@
 import io from "socket.io-client";
+import * as Actions from "../actions";
 
 const socketSendReceive = store => next => action => {
-    const socket = io();
-    next(action);
+    const socket = io("http://127.0.0.1:4001");
+
+    const { event, emit, handle, payload, ...rest } = action;
+
+    if (!event) {
+        return next(action);
+    }
+
+    let handleEvent = handle;
+    if (typeof handleEvent === "string") {
+        handleEvent = result =>
+            store.dispatch({ type: handle, result, ...rest });
+    }
+    socket.on(event, handleEvent);
+
+    if (emit) {
+        socket.emit(event, payload);
+        return;
+    }
 };
 
 export default socketSendReceive;
