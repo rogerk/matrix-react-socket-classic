1d61914e99aa59f6f171f785b353e3b56da843a3
rogerk <rkitain@yahoo.com>
2019-11-04 17:37:53 -0500

    - apply socket.io middleware

--- matrix/socket-io-client/src/js/store/index.js
+++ matrix/socket-io-client/src/js/store/index.js
@@ -1,8 +1,9 @@
-import socketSendReceive from "../middleware/socketSendReceive";
-import thunk from "redux-thunk";
+import socketIO from "socket.io-client";
+import socketIOMiddleware from "../middleware/socketIOMiddleware";
 import { createStore, applyMiddleware } from "redux";
 import Reducers from "../reducers";
 
-const store = createStore(Reducers, applyMiddleware(socketSendReceive, thunk));
+const io = socketIO.connect("http://127.0.0.1:4001");
+const store = createStore(Reducers, applyMiddleware(socketIOMiddleware(io)));
 
 export default store;
