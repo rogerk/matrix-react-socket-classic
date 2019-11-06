ae58fb97507030f1a01c843184c6aee21e1e5a17
rogerk <rkitain@yahoo.com>
2019-11-06 13:54:21 -0500

    - socket.io is handled in middleware

--- matrix/socket-io-client/src/js/components/container/MatrixContainer.jsx
+++ matrix/socket-io-client/src/js/components/container/MatrixContainer.jsx
@@ -1,42 +1,39 @@
 import { connect } from "react-redux";
 import React, { Component } from "react";
-import io from "socket.io-client";
 import Matrix from "../presentational/Matrix.jsx";
 import store from "../../store/index.js";
 import * as Actions from "../../actions";
 
 class MatrixContainer extends Component {
-    constructor(props) {
-        super();
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(props) {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick = event => {
-        const state = store.getState();
-        const color = state.color;
-        store.dispatch(
-            Actions.updatePixelColor({
-                pixel: event,
-                color: color
-            })
-        );
-    };
+  handleClick = event => {
+    const state = store.getState();
+    const color = state.color;
+    store.dispatch(
+      Actions.updatePixelColor({
+        pixel: event,
+        color: color
+      })
+    );
+  };
 
-    componentDidMount = () => {
-        store.dispatch(Actions.initialMatrix());
-    };
+  componentDidMount = () => {
+    store.dispatch(Actions.initialMatrix());
+  };
 
-    render = () => {
-        return (
-            <Matrix pixels={this.props.pixels} handleClick={this.handleClick} />
-        );
-    };
+  render = () => {
+    return <Matrix pixels={this.props.pixels} handleClick={this.handleClick} />;
+  };
 }
 
 const mapStateToProps = state => {
-    return {
-        pixels: state.pixels
-    };
+  return {
+    pixels: state.pixels
+  };
 };
 
 export default connect(mapStateToProps)(MatrixContainer);
