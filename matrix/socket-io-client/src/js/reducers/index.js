bbca6598a1b2db649ed3ee03e0c79cd7dca8d0be
rogerk <rkitain@yahoo.com>
2019-11-12 13:04:20 -0500

    - formatting.

--- matrix/socket-io-client/src/js/reducers/index.js
+++ matrix/socket-io-client/src/js/reducers/index.js
@@ -1,48 +1,50 @@
 import {
-  ALL_MATRIX,
-  ALL_MATRIX_FAILURE,
-  PIXEL_COLOR_UPDATE,
-  SET_COLOR,
-  MATRIX_COLOR_RESET
+    ALL_MATRIX,
+    ALL_MATRIX_FAILURE,
+    PIXEL_COLOR_UPDATE,
+    SET_COLOR,
+    MATRIX_COLOR_RESET
 } from "../constants/action-types";
 
 const initialState = {
-  pixels: [],
-  color: "#FFFFFF",
-  defaultColor: "#FFFFFF"
+    pixels: [],
+    color: "#FFFFFF",
+    defaultColor: "#FFFFFF"
 };
 
 export default (state = initialState, action) => {
-  if (action.type == ALL_MATRIX) {
-    return {
-      ...state,
-      pixels: action.result
-    };
-  } else if (action.type == ALL_MATRIX_FAILURE) {
-    console.log("Error: ", action.error);
-    return {
-      ...state,
-      error: action.error
-    };
-  } else if (action.type == PIXEL_COLOR_UPDATE) {
-    const id = action.result.id;
-    return {
-      ...state,
-      pixels: state.pixels.map(pixel =>
-        pixel.id === id ? { ...pixel, color: action.result.color } : pixel
-      )
-    };
-  } else if (action.type == MATRIX_COLOR_RESET) {
-    return {
-      ...state,
-      pixels: action.result
-    };
-  } else if (action.type == SET_COLOR) {
-    return {
-      ...state,
-      color: action.color.color
-    };
-  } else {
-    return state;
-  }
+    if (action.type == ALL_MATRIX) {
+        return {
+            ...state,
+            pixels: action.result
+        };
+    } else if (action.type == ALL_MATRIX_FAILURE) {
+        console.log("Error: ", action.error);
+        return {
+            ...state,
+            error: action.error
+        };
+    } else if (action.type == PIXEL_COLOR_UPDATE) {
+        const id = action.result.id;
+        return {
+            ...state,
+            pixels: state.pixels.map(pixel =>
+                pixel.id === id
+                    ? { ...pixel, color: action.result.color }
+                    : pixel
+            )
+        };
+    } else if (action.type == MATRIX_COLOR_RESET) {
+        return {
+            ...state,
+            pixels: action.result
+        };
+    } else if (action.type == SET_COLOR) {
+        return {
+            ...state,
+            color: action.color.color
+        };
+    } else {
+        return state;
+    }
 };
