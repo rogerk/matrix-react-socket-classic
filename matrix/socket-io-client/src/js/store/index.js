b28d079936e23bbb593fe1682b5c74291c0edba1
rogerk <rkitain@yahoo.com>
2019-11-12 15:56:57 -0500

    - package files: added redux-thunk - store index.js: added thunk middleware.

--- matrix/socket-io-client/src/js/store/index.js
+++ matrix/socket-io-client/src/js/store/index.js
@@ -1,9 +1,12 @@
 import socketIO from "socket.io-client";
 import socketIOMiddleware from "../middleware/socketIOMiddleware";
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 import Reducers from "../reducers";
 
+const middlewares = [thunk];
 const io = socketIO.connect("http://127.0.0.1:4001");
-const store = createStore(Reducers, applyMiddleware(socketIOMiddleware(io)));
+middlewares.push(socketIOMiddleware(io));
+const store = createStore(Reducers, applyMiddleware(...middlewares));
 
 export default store;
