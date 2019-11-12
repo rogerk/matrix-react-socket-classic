b81820433aa7a095ac08c2058a729d937c925c09
rogerk <rkitain@yahoo.com>
2019-11-12 13:05:57 -0500

    - formatting

--- matrix/socket-io-client/src/js/components/container/MatrixContainer.jsx
+++ matrix/socket-io-client/src/js/components/container/MatrixContainer.jsx
@@ -5,35 +5,37 @@ import store from "../../store/index.js";
 import * as Actions from "../../actions";
 
 class MatrixContainer extends Component {
-  constructor(props) {
-    super();
-    this.handleClick = this.handleClick.bind(this);
-  }
+    constructor(props) {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-  handleClick = event => {
-    const state = store.getState();
-    const color = state.color;
-    store.dispatch(
-      Actions.updatePixelColor({
-        pixel: event,
-        color: color
-      })
-    );
-  };
+    handleClick = event => {
+        const state = store.getState();
+        const color = state.color;
+        store.dispatch(
+            Actions.updatePixelColor({
+                pixel: event,
+                color: color
+            })
+        );
+    };
 
-  componentDidMount = () => {
-    store.dispatch(Actions.initialMatrix());
-  };
+    componentDidMount = () => {
+        store.dispatch(Actions.initialMatrix());
+    };
 
-  render = () => {
-    return <Matrix pixels={this.props.pixels} handleClick={this.handleClick} />;
-  };
+    render = () => {
+        return (
+            <Matrix pixels={this.props.pixels} handleClick={this.handleClick} />
+        );
+    };
 }
 
 const mapStateToProps = state => {
-  return {
-    pixels: state.pixels
-  };
+    return {
+        pixels: state.pixels
+    };
 };
 
 export default connect(mapStateToProps)(MatrixContainer);
