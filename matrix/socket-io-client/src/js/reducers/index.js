8440863d2e7a14a8191e80832e9933174f751288
rogerk <rkitain@yahoo.com>
2019-12-05 13:52:29 -0500

    - use switch instead of conditional if's

--- matrix/socket-io-client/src/js/reducers/index.js
+++ matrix/socket-io-client/src/js/reducers/index.js
@@ -13,41 +13,42 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-    if (action.type == ALL_MATRIX) {
-        return {
-            ...state,
-            pixels: action.result,
-            error: undefined
-        };
-    } else if (action.type == MATRIX_FAILURE) {
-        console.log("Error: ", action.error);
-        return {
-            ...state,
-            error: action.error
-        };
-    } else if (action.type == PIXEL_COLOR_UPDATE) {
-        const id = action.result.id;
-        return {
-            ...state,
-            pixels: state.pixels.map(pixel =>
-                pixel.id === id
-                    ? { ...pixel, color: action.result.color }
-                    : pixel
-            ),
-            error: undefined
-        };
-    } else if (action.type == MATRIX_COLOR_RESET) {
-        return {
-            ...state,
-            pixels: action.result,
-            error: undefined
-        };
-    } else if (action.type == SET_COLOR) {
-        return {
-            ...state,
-            color: action.color.color
-        };
-    } else {
-        return state;
+    switch (action.type) {
+        case ALL_MATRIX:
+            return {
+                ...state,
+                pixels: action.result,
+                error: undefined
+            };
+        case MATRIX_FAILURE:
+            console.log("Error: ", action.error);
+            return {
+                ...state,
+                error: action.error
+            };
+        case PIXEL_COLOR_UPDATE:
+            const id = action.result.id;
+            return {
+                ...state,
+                pixels: state.pixels.map(pixel =>
+                    pixel.id === id
+                        ? { ...pixel, color: action.result.color }
+                        : pixel
+                ),
+                error: undefined
+            };
+        case MATRIX_COLOR_RESET:
+            return {
+                ...state,
+                pixels: action.result,
+                error: undefined
+            };
+        case SET_COLOR:
+            return {
+                ...state,
+                color: action.color.color
+            };
+        default:
+            return state;
     }
 };
