5da13ef4c9174f81b5db101932976ce43cb66582
rogerk <rkitain@yahoo.com>
2019-12-19 17:56:37 -0500

    - added performance for bundle size optimization.

--- matrix/socket-io-client/build-utils/webpack.prod.js
+++ matrix/socket-io-client/build-utils/webpack.prod.js
@@ -2,6 +2,11 @@ const Dotenv = require("dotenv-webpack");
 
 module.exports = {
     mode: "production",
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
     devtool: "source-map",
     plugins: [
         new Dotenv({
